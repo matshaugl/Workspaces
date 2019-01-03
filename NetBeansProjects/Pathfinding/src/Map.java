3d642072366bd0ea2a2c1c8804c798235aa6b2b1
matshaugl <matshaugl@gmail.com>
2019-01-03 21:37:49 +0100

    test

--- NetBeansProjects/Pathfinding/src/Map.java
+++ NetBeansProjects/Pathfinding/src/Map.java
@@ -10,26 +10,26 @@ import org.newdawn.slick.util.pathfinding.PathFindingContext;
 import org.newdawn.slick.util.pathfinding.TileBasedMap;
 
 public class Map implements TileBasedMap {
-    
+
     Image mapPng;
     Image renderMap;
     Image tileSprite;
     SpriteSheet tiles;
-    
-    public void init(){
+
+    public void init() {
         Graphics g = null;
         try {
-            mapPng = new Image ("res/map.png", false, Image.FILTER_NEAREST);
-            tileSprite = new Image ("res/mapSheet.png", false, Image.FILTER_NEAREST);
-            renderMap = new Image(20*32, 20*32);
+            mapPng = new Image("res/map.png", false, Image.FILTER_NEAREST);
+            tileSprite = new Image("res/mapSheet.png", false, Image.FILTER_NEAREST);
+            renderMap = new Image(20 * 32, 20 * 32);
             g = renderMap.getGraphics();
         } catch (SlickException ex) {
             Logger.getLogger(Map.class.getName()).log(Level.SEVERE, null, ex);
         }
-        tiles = new SpriteSheet(tileSprite,32,32);
-        for(int x=0; x<20; x++){
-            for(int y=0; y<20; y++){
-                if(mapPng.getColor(x, y).equals(Color.black)){
+        tiles = new SpriteSheet(tileSprite, 32, 32);
+        for (int x = 0; x < 20; x++) {
+            for (int y = 0; y < 20; y++) {
+                if (mapPng.getColor(x, y).equals(Color.black)) {
                     g.drawImage(tiles.getSprite(0, 0), x * 32, y * 32);
                 } else {
                     g.drawImage(tiles.getSprite(2, 0), x * 32, y * 32);
@@ -37,16 +37,15 @@ public class Map implements TileBasedMap {
             }
         }
         g.flush();
-        
-        
+
     }
-    
-    public void render(Graphics g){
+
+    public void render(Graphics g) {
         g.drawImage(renderMap, 0, 0);
     }
-    
-    public void update(){
-        
+
+    public void update() {
+
     }
 
     @Override
@@ -70,8 +69,8 @@ public class Map implements TileBasedMap {
     }
 
     @Override
-    public float getCost(PathFindingContext context, int tx, int ty) {    
+    public float getCost(PathFindingContext context, int tx, int ty) {
         return 1;
     }
-    
+
 }
