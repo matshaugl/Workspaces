a3933f592b7b921eebe34550e0052e5a6e9c0f7f
Mats <matshaugl@gmail.com>
2020-05-12 21:07:19 +0200

    aa

--- Heimeundervisning/BulletCollision/src/Play.java
+++ Heimeundervisning/BulletCollision/src/Play.java
@@ -1,4 +1,11 @@
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.newdawn.slick.*;
 
 import org.newdawn.slick.geom.Vector2f;
@@ -6,32 +13,52 @@ import org.newdawn.slick.state.*;
 
 public class Play extends BasicGameState {
 
-    
-    
+    ServerSocket ss;
+    Socket socket;
+    InputStream inputStream;
+    ObjectInputStream objectInputStream;
+    Message message;
+
     BulletList bulletList;
-    
+
     Map map;
     Player player;
-
 
     public Play(int state) {
 
     }
 
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        
+
         bulletList = new BulletList();
-        
+
         map = new Map();
         player = new Player();
+
+        try {
+            ss = new ServerSocket(7777);
+            socket = ss.accept(); // blocking call, this will wait until a connection is attempted on this port.
+            inputStream = socket.getInputStream();
+            objectInputStream = new ObjectInputStream(inputStream);
+
+            message = (Message) objectInputStream.readObject();
+
+            System.out.println(message.getText());
+
+            ss.close();
+            socket.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Play.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         g.setColor(Color.yellow);
-        
+
         player.render(g);
-        
+
         map.render(g);
 
         bulletList.render(g);
@@ -42,10 +69,9 @@ public class Play extends BasicGameState {
         Input input = gc.getInput();
 
         player.update(delta, input, map, bulletList);
-        
-        
+
         bulletList.update(delta, map);
-        
+
     }
 
     public int getID() {
---
+++ Heimeundervisning/NetworkGame/src/Player.java
@@ -0,0 +1,99 @@
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Vector2f;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mats
+ */
+public class Player {
+    
+    Circle player;
+    float playerX;
+    float playerY;
+    float lastPlayerY;
+    float lastPlayerX;
+    float playerSpeed;
+    
+    public Player(){
+        playerX = 800;
+        playerY = 400;
+        player = new Circle(playerX, playerY, 20);
+        playerSpeed = 0.3f;
+    }
+
+    void render(Graphics g) {
+        g.draw(player);
+    }
+
+    void update(int delta, Input input, Map map, BulletList bulletList) {
+        float mouseX;
+        float mouseY;
+        
+        
+        lastPlayerX = playerX;
+        lastPlayerY = playerY;
+
+        if (input.isKeyDown(input.KEY_W)) {
+            playerY = playerY - (playerSpeed * delta);
+            player.setCenterY(playerY);
+            for (int i = 0; i < map.getWalls().size(); i++) {
+                if (player.intersects(map.getWalls().get(i))) {
+                    playerY = lastPlayerY;
+                    player.setCenterY(playerY);
+                }
+            }
+
+        }
+
+        if (input.isKeyDown(input.KEY_S)) {
+            playerY = playerY + (playerSpeed * delta);
+            player.setCenterY(playerY);
+            for (int i = 0; i < map.getWalls().size(); i++) {
+                if (player.intersects(map.getWalls().get(i))) {
+                    playerY = lastPlayerY;
+                    player.setCenterY(playerY);
+                }
+            }
+        }
+
+        if (input.isKeyDown(input.KEY_A)) {
+            playerX = playerX - (playerSpeed * delta);
+            player.setCenterX(playerX);
+            for (int i = 0; i < map.getWalls().size(); i++) {
+                if (player.intersects(map.getWalls().get(i))) {
+                    playerX = lastPlayerX;
+                    player.setCenterX(playerX);
+                }
+            }
+        }
+
+        if (input.isKeyDown(input.KEY_D)) {
+            playerX = playerX + (playerSpeed * delta);
+            player.setCenterX(playerX);
+            for (int i = 0; i < map.getWalls().size(); i++) {
+                if (player.intersects(map.getWalls().get(i))) {
+                    playerX = lastPlayerX;
+                    player.setCenterX(playerX);
+                }
+            }
+        }
+
+        if (input.isMouseButtonDown(0)) {
+            mouseX = input.getMouseX();
+            mouseY = input.getMouseY();
+            Vector2f vector = new Vector2f(mouseX - playerX, mouseY - playerY);
+            vector.normalise();
+            bulletList.add(new Projectile(playerX, playerY, vector));
+        }
+    }
+    
+}
